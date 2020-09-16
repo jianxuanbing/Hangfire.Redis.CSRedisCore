68d733ef3f28031a4b2bf0b55dd499d1d189b8ed
jian玄冰 <jianxuanhuo1@126.com>
2020-09-16 16:43:20 +0800

    fix: 修复由于 poolsize 过小 导致单元测试失败问题

--- tests/Hangfire.Redis.CSRedisCore.Tests/Utils/RedisUtils.cs
+++ tests/Hangfire.Redis.CSRedisCore.Tests/Utils/RedisUtils.cs
@@ -5,7 +5,7 @@ namespace Hangfire.Redis.Tests
 {
     public static class RedisUtils
     {
-        public const string ConnectionString = "127.0.0.1:6379,defaultDatabase=1,connectTimeout=30000,poolsize=10";
+        public const string ConnectionString = "127.0.0.1:6379,defaultDatabase=1,connectTimeout=30000,poolsize=100";
 
         public static CSRedisClient RedisClient { get; }
 
